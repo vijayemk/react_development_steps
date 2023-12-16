68248bdbcdad737b8279338b7a1030b6e5fdf552
Szigony <vijayemk>
2023-12-16 12:58:07 +0530

    add .gitignore file and add count program in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [add, setAdd]=useState(0)
+
+  
+
+  const handleAdd=()=>{
+    setAdd(add +1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       count {add}
+      <button onClick={handleAdd}>+</button>
+    
     </div>
   );
 }
---
+++ src/readme.txt
@@ -0,0 +1,5 @@
+1.'npx create-react-app my-react-app'
+2.intial setup
+3.add .gitignore file
+4.'npm install'
+5.'npm start'
